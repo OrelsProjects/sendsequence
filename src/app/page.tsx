4d96842c957a0cb44aebf193e898e51808c6b23a
orelsprojects <orelsmail@gmail.com>
2024-05-07 19:58:20 +0300

    remove all next-intl

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
-import { useTranslations } from "next-intl";
-import Image from "next/image";
 
 export default function Home() {
-  const t = useTranslations("Index");
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
      
