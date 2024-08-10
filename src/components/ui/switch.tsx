268c30d98bc9f71214d927efc7b638f561918a6f
orelsprojects <orelsmail@gmail.com>
2024-08-10 20:49:42 +0300

    Fix switch

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -27,7 +27,7 @@ const Switch = React.forwardRef<
     ref,
   ) => {
     // Internal state to track checked status
-    const [isChecked, setIsChecked] = React.useState(props.defaultChecked);
+    const [isChecked, setIsChecked] = React.useState(props.defaultChecked || props.checked);
 
     return (
       <div
